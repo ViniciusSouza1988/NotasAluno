cacfafebfeca2482fd0340be9b76b5871fe2fe24
ViniciusSouza1988 <viniciusdesouzasilveira1988@gmail.com>
2023-03-26 00:39:15 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿int Nota1Trimestre = 0;
+Console.WriteLine("Nota 1 Trimestre é: ");
+Nota1Trimestre = Convert.ToInt32(Console.ReadLine());
+
+int Nota2Trimestre = 0;
+Console.WriteLine("Nota 1 Trimestre é: ");
+Nota2Trimestre = Convert.ToInt32(Console.ReadLine());
+
+int Nota3Trimestre = 0;
+Console.WriteLine("Nota 1 Trimestre é: ");
+Nota3Trimestre = Convert.ToInt32(Console.ReadLine());
+
+int Nota4Trimestre = 0;
+Console.WriteLine("Nota 1 Trimestre é: ");
+Nota4Trimestre = Convert.ToInt32(Console.ReadLine());
+
+int NotaFinal = (Nota1Trimestre + Nota2Trimestre + Nota3Trimestre + Nota4Trimestre) / 4;
+
+Console.WriteLine("A nota média foi:" + NotaFinal);
+
+if (NotaFinal >= 70)
+{
+    Console.WriteLine("O aluno foi APROVADO");
+}
+else
+{
+    Console.WriteLine("O aluno foi REPROVADO");
+}
+    
+
+
